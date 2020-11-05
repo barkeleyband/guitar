cb444b3b5de344ba8c0c3faf52af7e39add8ee1f
Jesse Millar <jesse.millar@microsoft.com>
2020-11-05 11:06:25 -0600

    Adding a song to my backlog

--- song-info.sh
+++ song-info.sh
@@ -5,8 +5,9 @@
 PAGE_OUTPUT=$(curl -s "$1" | pup -i 0)
 TRACK_TITLE=$(echo "$PAGE_OUTPUT" | grep "twitter:title" | cut -c 41- | rev | cut -c 3- | rev)
 TRACK_ARTIST=$(echo "$PAGE_OUTPUT" | grep "twitter:audio:artist_name" | cut -c 52- | rev | cut -c 3- | rev)
+ULTIMATE_GUITAR_QUERY=$(echo "$TRACK_ARTIST $TRACK_TITLE" | sed 's/ /+/g')
 
-OUTPUT_STRING="- [$TRACK_TITLE]($1) by $TRACK_ARTIST ([tab](https://www.ultimate-guitar.com/search.php?search_type=title&value=$TRACK_TITLE))"
+OUTPUT_STRING="- [$TRACK_TITLE]($1) by $TRACK_ARTIST ([tab](https://www.ultimate-guitar.com/search.php?title=$ULTIMATE_GUITAR_QUERY))"
 
 echo "$OUTPUT_STRING"
 
