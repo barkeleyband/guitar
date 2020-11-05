a2a7439d1e4d0d09941e7293f56519f65f4c0671
Jesse Millar <jesse.millar@microsoft.com>
2020-11-05 11:02:33 -0600

    Adding a song to my backlog

--- song-info.sh
+++ song-info.sh
@@ -3,7 +3,7 @@
 # NOTE: Needs pup (https://github.com/ericchiang/pup) installed
 
 PAGE_OUTPUT=$(curl -s "$1" | pup -i 0)
-TRACK_TITLE=$(echo "$PAGE_OUTPUT" | grep "twitter:title" | cut -c 40- | rev | cut -c 3- | rev)
+TRACK_TITLE=$(echo "$PAGE_OUTPUT" | grep "twitter:title" | cut -c 41- | rev | cut -c 3- | rev)
 TRACK_ARTIST=$(echo "$PAGE_OUTPUT" | grep "twitter:audio:artist_name" | cut -c 52- | rev | cut -c 3- | rev)
 
 OUTPUT_STRING="- [$TRACK_TITLE]($1) by $TRACK_ARTIST ([tab](https://www.ultimate-guitar.com/search.php?search_type=title&value=$TRACK_TITLE))"
