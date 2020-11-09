90f38170a573687442e36ea4c7d98709d9807ae5
Jesse Millar <jesse.millar@microsoft.com>
2020-11-09 12:38:29 -0600

    Automatically add new songs to a Spotify playlist

--- guitar.sh
+++ guitar.sh
@@ -3,4 +3,5 @@
 SONG=$(./song-info.sh "$1")
 awk -v s="$SONG" 'p{printf "\n%s", s; p=0} /^## Guitar Songs Backlog/{p=1} 1' README.md > TMP.md
 mv TMP.md README.md
+./add-to-spotify.sh "$1"
 ./commit.sh
