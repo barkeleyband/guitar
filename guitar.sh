9bcc79bdfa3516fcc1cd0b04fc4bdc15842a2bd1
Jesse Millar <jesse.millar@microsoft.com>
2020-11-10 14:25:27 -0600

    Preventing adding duplicate songs

--- guitar.sh
+++ guitar.sh
@@ -7,6 +7,13 @@ else
 	SONG="$1"
 fi
 
+TRACK_ID=$(./get-spotify-track-id.sh "$SONG")
+if sed -n '/Guitar Song Collection/,/Bass Song Collection/p' README.md | grep -q "$TRACK_ID"
+then
+	echo "Song already added to guitar list"
+	exit
+fi
+
 SONG_INFO=$(./song-info.sh "$SONG")
 awk -v s="$SONG_INFO" 'p{printf "\n%s", s; p=0} /^## Guitar Songs Backlog/{p=1} 1' README.md > TMP.md
 mv TMP.md README.md
